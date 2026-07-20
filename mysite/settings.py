81d79cf165684facfb237af21d920b528cc9e4d3
eryial-a <kyletn22@gmail.com>
2026-07-20 18:08:06 -0500

    posts

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o3gogq7@*5tinvq#=+pfu&f8i@sk%c*qt)!)$u)84i6^#5p0fc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
